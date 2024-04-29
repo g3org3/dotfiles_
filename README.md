9d8cfaa0f9e4f715a0bea64c4b53c38781826da2
george <7jagjag@gmail.com>
2024-04-29 16:23:26 +0200

    add more tools and refacto

--- README.md
+++ README.md
@@ -1,7 +1,49 @@
-# tools
+# Tools
+
+## List
 
 - https://github.com/junegunn/fzf
 - https://github.com/ogham/exa
 - https://github.com/sharkdp/bat
 - https://github.com/sharkdp/fd
 - https://github.com/neovim/neovim
+- https://github.com/BurntSushi/ripgrep
+- https://github.com/ajeetdsouza/zoxide
+- https://github.com/denisidoro/navi
+- https://direnv.net
+- https://github.com/pyenv/pyenv
+- https://github.com/jesseduffield/lazygit
+
+## Languages
+
+```sh
+# rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+```
+
+```sh
+# go
+wget https://go.dev/dl/go1.22.2.linux-amd64.tar.gz
+rm -rf /usr/local/go && tar -C /usr/local -xzf go1.22.2.linux-amd64.tar.gz
+```
+
+```sh
+# nodejs
+curl -fsSL https://fnm.vercel.app/install | bash
+```
+
+```sh
+#
+fnm install v20
+npm install -g tree-sitter-cli
+```
+
+```sh
+# direnv
+curl -sfL https://direnv.net/install.sh | bash
+```
+
+```sh
+# pyenv
+curl https://pyenv.run | bash
+```
---
+++ roles/bashrc/files/bashrc.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+alias l='clear; pwd; exa --icons --sort modified --group-directories-first -lh --git'
+alias la='l -a'
+alias b='bat --line-range :1000 --color always --theme base16'
+alias back='cd ..; l'
+
+eval "$(zoxide init bash)"
+
+## BAT
+export BAT_THEME="gruvbox-dark"
+
+## FZF
+eval "$(fzf --bash)"
+export FZF_CTRL_T_OPTS="
+  --walker-skip .git,node_modules,target
+  --preview 'bat -n --color=always {}'
+  --bind 'ctrl-/:change-preview-window(down|hidden|)'"
+# CTRL-/ to toggle small preview window to see the full command
+# CTRL-Y to copy the command into clipboard using pbcopy
+export FZF_CTRL_R_OPTS="
+  --preview 'echo {}' --preview-window up:3:hidden:wrap
+  --bind 'ctrl-/:toggle-preview'
+  --bind 'ctrl-y:execute-silent(echo -n {2..} | pbcopy)+abort'
+  --color header:italic
+  --header 'Press CTRL-Y to copy command into clipboard'"
+export FZF_TMUX_OPTS='-p80%,60%'
+export FZF_DEFAULT_COMMAND='fd --type f --strip-cwd-prefix'
+export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+source "~/.tinted-fzf-gruvbox.sh"
+
+## GO
+export PATH="$PATH:/usr/local/go/bin"
+
+## NODEJS
+export PATH="$PATH:/root/.local/share/fnm:./node_modules/.bin"
+eval "$(fnm env)"
+eval "$(fnm completions --shell bash)"

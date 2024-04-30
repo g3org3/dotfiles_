08e1aa0b008dd8f5764989224750a4eee7216e16
george <7jagjag@gmail.com>
2024-04-30 02:17:47 +0200

    astro

---
+++ roles/astronvim/files/community.lua
@@ -0,0 +1,19 @@
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
+-- AstroCommunity: import any community modules here
+-- We import this file in `lazy_setup.lua` before the `plugins/` folder.
+-- This guarantees that the specs are processed before any user plugins.
+
+---@type LazySpec
+return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.pack.lua" },
+  -- import/override with your plugins folder
+  { import = "astrocommunity.colorscheme.gruvbox-nvim" },
+  { import = "astrocommunity.pack.gleam" },
+  { import = "astrocommunity.pack.bash" },
+  { import = "astrocommunity.pack.astro" },
+  -- { import = "astrocommunity.pack.ansible" },
+  { import = "astrocommunity.pack.java" },
+  { import = "astrocommunity.pack.typescript" },
+}
